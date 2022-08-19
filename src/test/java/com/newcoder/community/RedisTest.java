e87c8f57f3df045c4d48f14adfd3d1f40562eca6
liugang-1024 <2356239104@qq.com>
2022-08-19 22:37:12 +0800

    本部分整合了Redis，并基于Redis完成了帖子、评论的评价功能，包括统计点赞状态、点赞数量等

---
+++ src/test/java/com/newcoder/community/RedisTest.java
@@ -0,0 +1,64 @@
+package com.newcoder.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class RedisTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testString(){
+        String redisKey = "test:count";
+        redisTemplate.opsForValue().set(redisKey,1);
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+    }
+
+    @Test
+    public void testHash(){
+        String key = "user";
+        redisTemplate.opsForHash().put(key,"id",1);
+        redisTemplate.opsForHash().put(key,"username","zhangsan");
+        System.out.println(redisTemplate.opsForHash().get(key,"id"));
+        System.out.println(redisTemplate.opsForHash().get(key,"username"));
+    }
+
+    @Test
+    public void testTransaction(){
+        Object obj = redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String redisKey = "test:tx";
+                //开启事务
+                operations.multi();
+
+                operations.opsForSet().add(redisKey,"zhangsan");
+                operations.opsForSet().add(redisKey,"lisi");
+                operations.opsForSet().add(redisKey,"wangwu");
+
+                System.out.println(operations.opsForSet().members(redisKey));
+
+                return operations.exec();
+
+            }
+        });
+        System.out.println(obj);
+    }
+
+
+}
